ea0aff5c23718a8637b4a0b98d819d41623b0a17
Catarina-de-PD <catarina.f.bonifacio@gmail.com>
2022-06-22 16:29:59 -0300

    Versão em aula

---
+++ frmMovimento.Designer.cs
@@ -0,0 +1,306 @@
+﻿
+namespace Prova3
+{
+    partial class frmMovimento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdEspecial = new System.Windows.Forms.RadioButton();
+            this.rdComum = new System.Windows.Forms.RadioButton();
+            this.txtSaldo = new System.Windows.Forms.TextBox();
+            this.txtLimite = new System.Windows.Forms.TextBox();
+            this.txtConta = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtMovimento = new System.Windows.Forms.DateTimePicker();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnLancar = new System.Windows.Forms.Button();
+            this.btnDepositar = new System.Windows.Forms.Button();
+            this.btnSacar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rdEspecial);
+            this.groupBox1.Controls.Add(this.rdComum);
+            this.groupBox1.Controls.Add(this.txtSaldo);
+            this.groupBox1.Controls.Add(this.txtLimite);
+            this.groupBox1.Controls.Add(this.txtConta);
+            this.groupBox1.Location = new System.Drawing.Point(21, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(621, 104);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(466, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Saldo";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(312, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Limite";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(17, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nr. Conta";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // rdEspecial
+            // 
+            this.rdEspecial.AutoSize = true;
+            this.rdEspecial.Enabled = false;
+            this.rdEspecial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rdEspecial.Location = new System.Drawing.Point(150, 60);
+            this.rdEspecial.Name = "rdEspecial";
+            this.rdEspecial.Size = new System.Drawing.Size(134, 24);
+            this.rdEspecial.TabIndex = 4;
+            this.rdEspecial.TabStop = true;
+            this.rdEspecial.Text = "Conta Especial";
+            this.rdEspecial.UseVisualStyleBackColor = true;
+            // 
+            // rdComum
+            // 
+            this.rdComum.AutoSize = true;
+            this.rdComum.Enabled = false;
+            this.rdComum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rdComum.Location = new System.Drawing.Point(150, 30);
+            this.rdComum.Name = "rdComum";
+            this.rdComum.Size = new System.Drawing.Size(129, 24);
+            this.rdComum.TabIndex = 3;
+            this.rdComum.TabStop = true;
+            this.rdComum.Text = "Conta Comum";
+            this.rdComum.UseVisualStyleBackColor = true;
+            // 
+            // txtSaldo
+            // 
+            this.txtSaldo.BackColor = System.Drawing.Color.White;
+            this.txtSaldo.Enabled = false;
+            this.txtSaldo.Location = new System.Drawing.Point(470, 60);
+            this.txtSaldo.Name = "txtSaldo";
+            this.txtSaldo.Size = new System.Drawing.Size(132, 20);
+            this.txtSaldo.TabIndex = 2;
+            // 
+            // txtLimite
+            // 
+            this.txtLimite.BackColor = System.Drawing.Color.White;
+            this.txtLimite.Enabled = false;
+            this.txtLimite.Location = new System.Drawing.Point(316, 60);
+            this.txtLimite.Name = "txtLimite";
+            this.txtLimite.Size = new System.Drawing.Size(132, 20);
+            this.txtLimite.TabIndex = 1;
+            this.txtLimite.TextChanged += new System.EventHandler(this.txtLimite_TextChanged);
+            // 
+            // txtConta
+            // 
+            this.txtConta.BackColor = System.Drawing.Color.White;
+            this.txtConta.Location = new System.Drawing.Point(21, 60);
+            this.txtConta.MaxLength = 10;
+            this.txtConta.Name = "txtConta";
+            this.txtConta.Size = new System.Drawing.Size(100, 20);
+            this.txtConta.TabIndex = 0;
+            this.txtConta.TextChanged += new System.EventHandler(this.txtConta_TextChanged_1);
+            this.txtConta.Leave += new System.EventHandler(this.txtConta_Leave);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.dtMovimento);
+            this.groupBox2.Controls.Add(this.txtValor);
+            this.groupBox2.Controls.Add(this.btnLancar);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(21, 219);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(621, 95);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Movimento:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(17, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Data";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(248, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Valor";
+            // 
+            // dtMovimento
+            // 
+            this.dtMovimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtMovimento.Location = new System.Drawing.Point(21, 48);
+            this.dtMovimento.Name = "dtMovimento";
+            this.dtMovimento.Size = new System.Drawing.Size(200, 20);
+            this.dtMovimento.TabIndex = 6;
+            this.dtMovimento.Leave += new System.EventHandler(this.dtMovimento_Leave);
+            // 
+            // txtValor
+            // 
+            this.txtValor.BackColor = System.Drawing.Color.White;
+            this.txtValor.Location = new System.Drawing.Point(252, 48);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(137, 20);
+            this.txtValor.TabIndex = 3;
+            this.txtValor.TextChanged += new System.EventHandler(this.txtValor_TextChanged);
+            this.txtValor.Leave += new System.EventHandler(this.txtValor_Leave);
+            // 
+            // btnLancar
+            // 
+            this.btnLancar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(157)))), ((int)(((byte)(169)))));
+            this.btnLancar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLancar.Location = new System.Drawing.Point(470, 25);
+            this.btnLancar.Name = "btnLancar";
+            this.btnLancar.Size = new System.Drawing.Size(142, 43);
+            this.btnLancar.TabIndex = 5;
+            this.btnLancar.Text = "Lançar";
+            this.btnLancar.UseVisualStyleBackColor = false;
+            this.btnLancar.Click += new System.EventHandler(this.btnLancar_Click);
+            // 
+            // btnDepositar
+            // 
+            this.btnDepositar.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnDepositar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDepositar.Location = new System.Drawing.Point(21, 134);
+            this.btnDepositar.Name = "btnDepositar";
+            this.btnDepositar.Size = new System.Drawing.Size(151, 68);
+            this.btnDepositar.TabIndex = 1;
+            this.btnDepositar.Text = "DEPOSITAR";
+            this.btnDepositar.UseVisualStyleBackColor = false;
+            this.btnDepositar.Click += new System.EventHandler(this.btnDepositar_Click);
+            // 
+            // btnSacar
+            // 
+            this.btnSacar.BackColor = System.Drawing.Color.LightPink;
+            this.btnSacar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSacar.Location = new System.Drawing.Point(192, 134);
+            this.btnSacar.Name = "btnSacar";
+            this.btnSacar.Size = new System.Drawing.Size(151, 68);
+            this.btnSacar.TabIndex = 2;
+            this.btnSacar.Text = "SACAR";
+            this.btnSacar.UseVisualStyleBackColor = false;
+            this.btnSacar.Click += new System.EventHandler(this.btnSacar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(157)))), ((int)(((byte)(169)))));
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(491, 134);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(151, 68);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmMovimento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(669, 326);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnSacar);
+            this.Controls.Add(this.btnDepositar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMovimento";
+            this.Text = "Movimento de Conta";
+            this.Load += new System.EventHandler(this.frmMovimento_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdEspecial;
+        private System.Windows.Forms.RadioButton rdComum;
+        private System.Windows.Forms.TextBox txtSaldo;
+        private System.Windows.Forms.TextBox txtLimite;
+        private System.Windows.Forms.TextBox txtConta;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtMovimento;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnLancar;
+        private System.Windows.Forms.Button btnDepositar;
+        private System.Windows.Forms.Button btnSacar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ frmMovimento.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prova3
+{
+    public partial class frmMovimento : Form
+    {
+        bool existe = false;
+        bool deposito;
+        bool saque;
+        static List<contaBancaria> _listConta = new List<contaBancaria>();
+        private contaBancaria _contaAtual = null;
+        private int operacao = 0;
+        public frmMovimento()
+        {
+            InitializeComponent();
+            this.Size = new Size(685, 255);
+            _listConta = contaBancaria.geraContas();
+        }
+        private void frmMovimento_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdComum_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtConta_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void txtConta_EnabledChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void txtConta_CursorChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMovimento_Leave(object sender, EventArgs e)
+        {
+        }
+
+        private void btnDepositar_Click(object sender, EventArgs e)
+        {
+            if (existe)
+            {
+                this.Size = new Size(685, 365);
+                saque = false;
+                deposito = true;
+                txtConta.Enabled = false;
+                txtLimite.Enabled = false;
+                txtSaldo.Enabled = false;
+                rdComum.Enabled = false;
+                rdEspecial.Enabled = false;
+                btnDepositar.Enabled = false;
+                btnSair.Enabled = false;
+                btnSacar.Enabled = false;
+            }
+        }
+
+        private void txtConta_Leave(object sender, EventArgs e)
+        {
+
+            string nconta = Convert.ToString(txtConta.Text.Trim());
+            foreach (contaBancaria contaBancaria in _listConta)
+            {
+                if (contaBancaria.CodigoConta.Equals(nconta))
+                {
+                    txtSaldo.Text = contaBancaria.Saldo.ToString();
+                    txtLimite.Text = contaBancaria.Limite.ToString();
+                    if (contaBancaria.Tipo == 0)
+                    {
+                        rdComum.Checked = true;
+                    }
+                    else
+                    {
+                        rdEspecial.Checked = true;
+                    }
+                    this.Size = new Size(685, 365);
+                    existe = true;
+                    break;
+                }
+            }
+            if (!existe)
+            {
+                this.Size = new Size(685, 255);
+                MessageBox.Show("Conta não encontrada!", "Aviso do sistema");
+                txtConta.Select();
+            }
+        }
+
+        private void dtMovimento_Leave(object sender, EventArgs e)
+        {
+            if ((dtMovimento.Value.DayOfWeek == DayOfWeek.Sunday) ||
+                (dtMovimento.Value.DayOfWeek == DayOfWeek.Saturday))
+            {
+                MessageBox.Show("Data Inválida!");
+                dtMovimento.Focus();
+            }
+        }
+
+        private void txtValor_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToDouble(txtValor.Text.Trim()) < 0)
+                {
+                    MessageBox.Show("Erro! Valores negativos não são aceitos", "Aviso do sistema");
+                    txtValor.Focus();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro no valor inserido (apenas números são aceitos)", "Erro");
+            }
+        }
+
+        private void btnLancar_Click(object sender, EventArgs e)
+        {
+            foreach (contaBancaria contaBancaria in _listConta)
+            {
+                string nconta = Convert.ToString(txtConta.Text.Trim());
+                if (contaBancaria.CodigoConta.Equals(nconta))
+                {
+                    if (deposito)
+                    {
+                        contaBancaria.Saldo += Convert.ToDouble(txtValor.Text.Trim());
+                        txtSaldo.Text = Convert.ToString(contaBancaria.Saldo);
+                        MessageBox.Show("Valor atualizado");
+                        break;
+                    }
+
+                    if (saque)
+                    {
+                        if (contaBancaria.Tipo == 0)
+                        {
+                            if (Convert.ToDouble(txtValor.Text.Trim()) > contaBancaria.Saldo)
+                            {
+                                MessageBox.Show("Erro! Valor selecionado maior que saldo", "Erro");
+                            }
+                            else
+                            {
+                                contaBancaria.Saldo -= Convert.ToDouble(txtValor.Text.Trim());
+                                txtSaldo.Text = Convert.ToString(contaBancaria.Saldo);
+                                MessageBox.Show("Valor atualizado");
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if (Convert.ToDouble(txtValor.Text.Trim()) > contaBancaria.Saldo + contaBancaria.Limite)
+                            {
+                                MessageBox.Show("Erro! Valor selecionado excedeu limite", "Erro");
+                            }
+                            else
+                            {
+                                contaBancaria.Saldo -= Convert.ToDouble(txtValor.Text.Trim());
+                                txtSaldo.Text = Convert.ToString(contaBancaria.Saldo);
+                                MessageBox.Show("Valor atualizado");
+                                break;
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            this.Size = new Size(685, 255);
+            txtValor.Clear();
+            txtLimite.Enabled = false;
+            txtSaldo.Enabled = false;
+            rdComum.Enabled = false;
+            rdEspecial.Enabled = false;
+            btnDepositar.Enabled = true;
+            btnSair.Enabled = true;
+            btnSacar.Enabled = true;
+            txtConta.Enabled = true;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnSacar_Click(object sender, EventArgs e)
+        {
+            if (existe)
+            {
+                this.Size = new Size(685, 365);
+                saque = true;
+                deposito = false;
+                txtConta.Enabled = false;
+                txtLimite.Enabled = false;
+                txtSaldo.Enabled = false;
+                rdComum.Enabled = false;
+                rdEspecial.Enabled = false;
+                btnDepositar.Enabled = false;
+                btnSair.Enabled = false;
+                btnSacar.Enabled = false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtConta_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSacar_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtValor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLimite_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
